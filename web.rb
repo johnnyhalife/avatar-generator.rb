48cc2f6d69f76b04b1a88226b702f87a7513afea
Johnny Halife <johnny@mural.ly>
2014-01-24 11:20:51 -0300

    remove null and empties from the array

--- web.rb
+++ web.rb
@@ -4,14 +4,14 @@ require 'rvg/rvg'
 require 'redcarpet'
 
 get '/' do
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)  
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
   markdown.render(File.read('./README.md'))
 end
 
 # matches up-to /{0:size}/{2:background}/{3:foreground}.{4:format}
 get /([^\/]+)(\/([^\/]+)\/([^\/]+))?\.(\w+)/ do |size, _, background, foreground, format|
   cache_control :public, :max_age => 36000
-  
+
   content_type "image/#{format}"
   opts = { :full_mode => params[:mode] == 'full' }
   to_image(size, background, foreground, format, params[:text], opts)
@@ -31,10 +31,10 @@ def to_image(size, background, foreground, format = 'png', text = nil, opts = {}
   background = background ? "##{background}" : 'black'
   foreground = foreground ? "##{foreground}" : 'grey69'
 
-  if (text && !opts[:full_mode]) then 
-    text = text.split(/\s/).map { |t| t[0].upcase }.slice(0, 3).join('')
+  if (text && !opts[:full_mode]) then
+    text = (text.split(/\s/)- ["", nil]).map { |t| t[0].upcase }.slice(0, 3).join('')
   end
-  
+
   w, h = size.split('x').map { |d| d.to_i }
   h ||= w
 
